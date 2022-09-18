6ba05dcbb06f9050205bf40262a6b99d44486d06
Diego Murillo Ferrer <diegomurilloferrer@gmail.com>
2022-09-18 14:56:59 -0500

    Add quotes data

---
+++ src/utils/quotesData.js
@@ -0,0 +1,36 @@
+const quotesData = [
+    {
+        author: "Gloria Pitzer",
+        text: "About the only thing that comes to us without effort is old age.",
+    },
+    {
+        author: "Brooks Atkinson",
+        text: "The perfect bureaucrat everywhere is the man who manages to make no decisions and escape all responsibility.",
+    },
+    {
+        author: "Pearl Buck",
+        text: "At my age the bones are water in the morning until food is given them.",
+    },
+    {
+        author: "Bonnie Prudden",
+        text: "You can't turn back the clock. But you can wind it up again.",
+    },
+    {
+        author: "Jewish Proverb",
+        text: "Rejoice not at thine enemy's fall - but don't rush to pick him up either.",
+    },
+    {
+        author: "Sydney J. Harris",
+        text: "An idealist believes the short run doesn't count. A cynic believes the long run doesn't matter. A realist believes that what is done or left undone in the short run determines the long run.",
+    },
+    {
+        author: "Laurie Anderson",
+        text: "Paradise is exactly like where you are right now... only much, much better.",
+    },
+    {
+        author: "John Updike",
+        text: "America is a vast conspiracy to make you happy.",
+    },
+];
+
+export default quotesData;
